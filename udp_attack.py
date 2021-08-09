fbf8e1fce05454cfe3e15badbdbdff56fd6886da
jimluo <jim.luo@nrl.navy.mil>
2021-08-09 11:40:07 -0700

    fixed udp attack

--- udp_attack.py
+++ udp_attack.py
@@ -1,7 +1,12 @@
 import socket
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-    sock.connect(("192.168.80.90", 5002))
+    sock.connect(("192.168.80.91", 5002))
 
-    sock.send(b"hi from client")
-    print(sock.recvfrom(8192))
+    i = 0;
+    while True:
+      i += 1
+      sock.send(b"hi from client")
+      if i == 1000:
+        print('sent 1000 packets ')
+        i = 0
